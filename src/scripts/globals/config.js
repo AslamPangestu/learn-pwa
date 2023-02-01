f679da79a7e6be3bc3a0c4720aa7ce6fb2657644
aslampangestu <aslampangestu@gmail.com>
2023-02-01 17:07:18 +0700

    caching

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,8 @@ const CONFIG = {
   KEY: 'YOUR_API_KEY',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us'
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString()
 }
 
 export default CONFIG
