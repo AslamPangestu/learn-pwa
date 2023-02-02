319d1a3f730ad14b06ef3f587e6178da98da58dd
aslampangestu <aslampangestu@gmail.com>
2023-02-02 09:17:15 +0700

    push notif with workbox

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,7 @@
 const { merge } = require('webpack-merge')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
+
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
@@ -19,5 +22,11 @@ module.exports = merge(common, {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
+      swDest: './sw.bundle.js'
+    })
+  ]
 })
