4db26db53d50918652d174ce416898cc0e874b37
aslampangestu <aslampangestu@gmail.com>
2023-02-02 08:45:42 +0700

    idb

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,11 +1,13 @@
 import NowPlaying from '../views/pages/now-playing'
 import Upcoming from '../views/pages/upcoming'
 import Detail from '../views/pages/detail'
+import Favourites from '../views/pages/favourites'
 
 const routes = {
   '/': NowPlaying, // default page
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
+  '/favourites': Favourites,
   '/detail/:id': Detail
 }
 
--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import UrlParser from '../../routes/url-parser'
 import TheMovieDbSource from '../../data/themoviedb-source'
 import { createMovieDetailTemplate } from '../templates/template-creator'
+import LikeButtonInitiator from '../../utils/like-button-initiator'
 
 const Detail = {
   async render () {
     return `
       <div id="movie" class="movie"></div>
+      <div id="likeButtonContainer"></div>
     `
   },
 
@@ -14,6 +16,16 @@ const Detail = {
     const movie = await TheMovieDbSource.detailMovie(url.id)
     const movieContainer = document.querySelector('#movie')
     movieContainer.innerHTML = createMovieDetailTemplate(movie)
+    LikeButtonInitiator.init({
+      overview: movie.overview,
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average
+      }
+    })
   }
 }
 
